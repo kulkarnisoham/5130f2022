1237e176ff82f4bae34b7402976bc921913c05bd
kulkarnisoham <kulkarni.soham03101998@gmail.com>
2022-11-01 22:16:55 -0400

    IWS Project Progress Week 1

---
+++ IWS-Project/config.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    API_KEY = os.environ.get('API_KEY')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_PORT= os.environ.get('MAIL_PORT')
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
